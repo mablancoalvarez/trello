72546b97544b5cbadf8088b7aa0bf58888cd4127
mablancoalvarez <mablancoalvarez@gmail.com>
2020-05-05 12:57:37 +0200

    fix button

--- public/assets/js/newlist.js
+++ public/assets/js/newlist.js
@@ -1,15 +1,101 @@
 'use strict';
-function add (){
+
+const containerCard1 = document.querySelector('container-list1');
+const containerCard2 = document.querySelector('container-list2');
+const containerCard3 = document.querySelector('container-list3');
+
+
+// const add = function (event){
+// console.log(event.target)
+//     const element = document.createElement('article');
    
-    const element = document.createElement('div');
-    const text = document.createTextNode('hola');
-    element.appendChild(text);
-    element.setAttribute('class','js-card app-card m-1 mb-2 p-2 bg-white rounded-sm app-cursor-pointer shadow-sm');
-    document.getElementById('container-list').appendChild(element)
-};
+//     element.setAttribute('class','js-card app-card m-1 mb-2 p-2 bg-white rounded-sm app-cursor-pointer shadow-sm');
+//     element.setAttribute('id','main-div');
+    
+//     mainContainer.appendChild(element);
+   
+//      const element2 = document.createElement('div');
+//      const span1= document.createElement('span');
+//      const span2 = document.createElement('span');
+//      const span3 = document.createElement('span');
+
+//      const span1text = document.createTextNode('JS');
+//      const span2text = document.createTextNode('Css');
+//      const span3text = document.createTextNode('Html');
+
+//     element2.appendChild(span1);
+//     element2.appendChild(span2);
+//     element2.appendChild(span3);
+
+//     span1.setAttribute('class','badge badge-secondary bg-success');
+//     span2.setAttribute('class','badge badge-secondary bg-success');
+//     span3.setAttribute('class','badge badge-secondary bg-success');
+
+//     span1.appendChild(span1text);
+//     span2.appendChild(span2text);
+//     span3.appendChild(span3text);
+    
+//     element.appendChild(element2)
+
+//     const text = document.createTextNode('Pintar card');
+//     element.appendChild(text);
+//     // const element3 = document.createElement('h3');
+//     // element3.setAttribute('class','h6');
+//     // text = document.createTextNode('Ejemplo');
+//     // element3.appendChild(text);
+//     // document.getElementById('main-div').appendChild(element3);
+    
+// };
 
 
-document.querySelectorAll('.ml-1').forEach(btn => {
-    btn.addEventListener('click', add);
+document.querySelectorAll('.app-list').forEach(item => {
+   const mainContainer = item.querySelector('.p-1.rounded-sm.bg-primary.shadow')
+
+    const buttonadd = item.querySelector('.ml-1')
+    buttonadd.addEventListener('click', add)
+ function add(){
+      
+            const element = document.createElement('article');
+
+            element.setAttribute('class','js-card app-card m-1 mb-2 p-2 bg-white rounded-sm app-cursor-pointer shadow-sm');
+            element.setAttribute('id','main-div');
+            
+            mainContainer.appendChild(element);
+           
+             const element2 = document.createElement('div');
+             const span1= document.createElement('span');
+             const span2 = document.createElement('span');
+             const span3 = document.createElement('span');
+        
+             const span1text = document.createTextNode('JS');
+             const span2text = document.createTextNode('Css');
+             const span3text = document.createTextNode('Html');
+        
+            element2.appendChild(span1);
+            element2.appendChild(span2);
+            element2.appendChild(span3);
+        
+            span1.setAttribute('class','badge badge-secondary bg-success');
+            span2.setAttribute('class','badge badge-secondary bg-success');
+            span3.setAttribute('class','badge badge-secondary bg-success');
+        
+            span1.appendChild(span1text);
+            span2.appendChild(span2text);
+            span3.appendChild(span3text);
+            
+            element.appendChild(element2)
+        
+            const text = document.createTextNode('Pintar card');
+            element.appendChild(text);
+            // const element3 = document.createElement('h3');
+            // element3.setAttribute('class','h6');
+            // text = document.createTextNode('Ejemplo');
+            // element3.appendChild(text);
+            // document.getElementById('main-div').appendChild(element3);
+            
+        };
+        
+
   });
+
 //# sourceMappingURL=newlist.js.map
